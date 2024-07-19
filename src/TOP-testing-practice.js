29583fc593393d023a3439d57d56fe0d77990f72
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-07-19 19:27:12 -0400

    feature: initialize `reverseString` module and corresponding test file

--- src/TOP-testing-practice.js
+++ src/TOP-testing-practice.js
@@ -10,6 +10,8 @@ export default {
 
     return first.toUpperCase() + rest.join('');
   },
+  reverseString: (string) => {
+  },
 
 }
 
---
+++ src/tests/TOP-testing-practice/reverse-string.test.js
@@ -0,0 +1,28 @@
+import topTestingPractice from "../../top-testing-practice";
+
+// Test function
+const reverseString = topTestingPractice.reverseString;
+
+// basic test cases
+const basicTestCases = [
+  { input: '', expected: ''},
+  { input: '', expected: ''},
+  { input: '', expected: ''},
+];
+
+describe('reverseString()', () => {
+
+  describe('basic functionality', () => {
+    test('checks for the existence of `reverseString`', () => {
+      expect(reverseString).toBeDefined();
+    });
+
+  });
+
+
+
+
+
+
+
+});
